d080122eb3f0ea5f4d2d1ad6b3e0b8f7c2cdc8d6
Tony Costa <antonio.costa@cubos.io>
2019-12-10 14:38:09 -0300

    Convites

--- app/Controllers/Http/InviteController.js
+++ app/Controllers/Http/InviteController.js
@@ -1,16 +1,21 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Invite = use('App/Models/Invite')
 
 /**
  * Resourceful controller for interacting with invites
  */
 class InviteController {
-  async store ({ request, response }) {
-    console.log(request.team)
+  async store ({ request, auth }) {
+    const invites = request.input('invites')
 
+    const data = invites.map(email => ({
+      email,
+      user_id: auth.user.id,
+      team_id: request.team.id
+    }))
+
+    await Invite.createMany(data)
   }
 
 }
