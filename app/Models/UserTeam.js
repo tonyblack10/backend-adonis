f8c57f482f4daa7850e5924cd3333a9c14052a81
Tony Costa <antonio.costa@cubos.io>
2019-12-10 16:15:32 -0300

    Setup Autorização

--- app/Models/UserTeam.js
+++ app/Models/UserTeam.js
@@ -4,8 +4,23 @@
 const Model = use('Model')
 
 class UserTeam extends Model {
+  static get traits () {
+    return [
+      '@provider:Adonis/Acl/HasRole',
+      '@provider:Adonis/Acl/HasPermission'
+    ]
+  }
+
   user () {
     return this.belongsTo('App/Models/User')
+  }
+
+  roles () {
+    return this.belongsToMany('Adonis/Acl/Role')
+  }
+
+  permission () {
+    return this.belongsToMany('Adonis/Acl/Permission')
   }
 }
 
